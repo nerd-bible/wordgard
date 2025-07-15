d2328f5c4927e2e2c8fdc637c3a4325b6e9fdb5f
Marijn Haverbeke <marijn@haverbeke.berlin>
2025-07-15 10:42:52 +0200

    Fix regression in serializeFragment

--- doc/src/to_dom.ts
+++ doc/src/to_dom.ts
@@ -76,7 +76,7 @@ export function serializeSlice(slice: Slice, options: SerializeOptions & {
       stack.pop()
     } else {
       let outer, content
-      if (contextDepth <= context.length) {
+      if (contextDepth >= context.length) {
         outer = content = opts.document.createElement("div")
       } else {
         ;({outer, content} = serializeNodeMarkup(context[contextDepth++], opts))
