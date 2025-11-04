9413be3840735dd36e175100306b76ed6a00a95d
Marijn Haverbeke <marijn@haverbeke.berlin>
2025-11-04 11:22:13 +0100

    Continue working on menu

--- menu/src/item.ts
+++ menu/src/item.ts
@@ -1,79 +1,162 @@
 import {Command} from "@wordgard/view"
 import {EditorState} from "@wordgard/state"
 
-export type MenuItem = {
-  run: Command
-  // FIXME feed these some pre-processed data about selection to avoid duplicate work?
+export type MenuLabel = string | {icon: string}
+
+export interface BaseMenuItem {
+  type: string
   select?: (state: EditorState) => boolean
   enable?: (state: EditorState) => boolean
+  parent?: MenuItem
+  rank?: number
+}
+
+export interface MenuButton extends BaseMenuItem {
+  type: "button"
+  run: Command
   active?: (state: EditorState) => boolean
   // FIXME use 100x100 viewport for these, find better icons
-  label: string | {icon: string}
+  label: MenuLabel
   description: string
-  group: MenuGroup
-  precedence: number
-  // FIXME specify hotkey here and allow menu items to provide the key bindings?
 }
 
-export class MenuCollection {
-  constructor(spec: {} = {}) {}
+export interface MenuGroup extends BaseMenuItem {
+  type: "group"
+  margin?: boolean
 }
 
-export class MenuDropDown {
-  constructor(spec: {} = {}) {}
+export interface MenuSelect extends BaseMenuItem {
+  type: "select"
+  description: string
+  label?: MenuLabel
+  defaultLabel: MenuLabel
 }
 
-export type MenuGroup = MenuCollection | MenuDropDown
+// FIXME make this easily extendable in 3rd party code, somehow
+export type MenuItem = MenuButton | MenuGroup | MenuSelect
 
-export const Commands = new MenuCollection()
-export const InlineStyles = new MenuCollection()
-export const Alignment = new MenuCollection()
+export class TemplateGroup {
+  parent: MenuItem | null
+  rank: number
 
-export const staticMenu: MenuItem[] = [ // FIXME automatic generation
-  {
-    run: () => { console.log("undo"); return true },
-    label: {icon: "M8 3a5 5 0 1 1-4.55 2.91.5.5 0 0 0-.91-.42A6 6 0 1 0 8 2z M8 4.47V.534.25.25 0 0 0-.41-.19L5.23 2.31a.25.25 0 0 0 0 .38l2.36 1.97A.25.25 0 0 0 8 4.47"},
-    description: "Undo",
-    group: Commands,
-    precedence: 10
-  },
-  {
-    run: () => { console.log("redo"); return true },
-    label: {icon: "M8 3a5 5 0 1 0 4.55 2.91.5.5 0 0 1 .91-.42A6 6 0 1 1 8 2z M8 4.47V.53a.25.25 0 0 1 .41-.19l2.36 1.97c.12.1.12.28 0 .38L8.41 4.66A.25.25 0 0 1 8 4.47"},
-    description: "Redo",
-    group: Commands,
-    precedence: 20
-  },
-  {
-    run: () => { console.log("strong"); return true },
-    label: {icon: "M8.21 13c2.11 0 3.41-1.09 3.41-2.82 0-1.31-.98-2.28-2.32-2.39v-.06a2.18 2.18 0 0 0 1.85-2.14c0-1.51-1.16-2.46-3.014-2.46H3.843V13zM5.91 4.67h1.70c.96 0 1.52.45 1.52 1.24 0 .83-.63 1.32-1.73 1.32H5.91V4.67zm0 6.79V8.60h1.73c1.22 0 1.88.49 1.88 1.41 0 .94-.64 1.45-1.83 1.45H5.91z"},
-    description: "Toggle strong emphasis",
-    group: InlineStyles,
-    precedence: 10
-  },
-  {
-    run: () => { console.log("left"); return true },
-    label: {icon: "M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"},
-    description: "Align left",
-    group: Alignment,
-    precedence: 10
-  },
-  {
-    run: () => { console.log("right"); return true },
-    label: {icon: "M6 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-4-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m4-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-4-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"},
-    description: "Align right",
-    group: Alignment,
-    precedence: 20
-  },
-  {
-    run: () => { console.log("center"); return true },
-    label: {icon: "M4 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"},
-    description: "Center",
-    group: Alignment,
-    precedence: 30
-  },
-]
+  private constructor(readonly item: MenuItem, readonly content: readonly (MenuItem | "...")[] = []) {
+    this.parent = item.parent ?? null
+    this.rank = item.rank ?? 100
+  }
 
-export function groupItems(items: readonly MenuItem[], groups: readonly MenuGroup[]) {
-  
+  static of(item: MenuItem, ...content: (MenuItem | "...")[]) {
+    return new TemplateGroup(item, content.length ? content : ["..."])
+  }
+}
+
+export const Top: MenuGroup = {type: "group"}
+export const Commands: MenuGroup = {type: "group", parent: Top, rank: 10}
+export const InlineStyles: MenuGroup = {type: "group", parent: Top, rank: 30}
+
+export const Alignment: MenuSelect = {
+  type: "select",
+  parent: Top,
+  defaultLabel: {icon: "M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"},
+  rank: 50,
+  description: "Text alignment"
+}
+
+export const Undo: MenuButton = {
+  type: "button",
+  run: () => { console.log("undo"); return true },
+  label: {icon: "M8 3a5 5 0 1 1-4.55 2.91.5.5 0 0 0-.91-.42A6 6 0 1 0 8 2z M8 4.47V.534.25.25 0 0 0-.41-.19L5.23 2.31a.25.25 0 0 0 0 .38l2.36 1.97A.25.25 0 0 0 8 4.47"},
+  description: "Undo",
+  parent: Commands,
+  rank: 10
+}
+
+export const Redo: MenuButton = {
+  type: "button",
+  run: () => { console.log("redo"); return true },
+  label: {icon: "M8 3a5 5 0 1 0 4.55 2.91.5.5 0 0 1 .91-.42A6 6 0 1 1 8 2z M8 4.47V.53a.25.25 0 0 1 .41-.19l2.36 1.97c.12.1.12.28 0 .38L8.41 4.66A.25.25 0 0 1 8 4.47"},
+  description: "Redo",
+  parent: Commands,
+  rank: 20
+}
+
+export const Strong: MenuButton = {
+  type: "button",
+  run: () => { console.log("strong"); return true },
+  label: {icon: "M8.21 13c2.11 0 3.41-1.09 3.41-2.82 0-1.31-.98-2.28-2.32-2.39v-.06a2.18 2.18 0 0 0 1.85-2.14c0-1.51-1.16-2.46-3.014-2.46H3.843V13zM5.91 4.67h1.70c.96 0 1.52.45 1.52 1.24 0 .83-.63 1.32-1.73 1.32H5.91V4.67zm0 6.79V8.60h1.73c1.22 0 1.88.49 1.88 1.41 0 .94-.64 1.45-1.83 1.45H5.91z"},
+  description: "Toggle strong emphasis",
+  parent: InlineStyles,
+  rank: 10
+}
+
+// FIXME use start/end nomenclature?
+export const AlignLeft: MenuButton = {
+  type: "button",
+  run: () => { console.log("left"); return true },
+  label: {icon: "M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"},
+  description: "Align left",
+  parent: Alignment,
+  rank: 10
+}
+
+export const AlignRight: MenuButton = {
+  type: "button",
+  run: () => { console.log("right"); return true },
+  label: {icon: "M6 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-4-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m4-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-4-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"},
+  description: "Align right",
+  parent: Alignment,
+  rank: 20
+}
+
+export const AlignCenter: MenuButton = {
+  type: "button",
+  run: () => { console.log("center"); return true },
+  label: {icon: "M4 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"},
+  description: "Center",
+  parent: Alignment,
+  active: () => true,
+  rank: 30
+}
+
+// FIXME automatic generation from facet
+export const staticMenu: MenuItem[] = [Undo, Redo, Strong, AlignLeft, AlignRight, AlignCenter, Commands, InlineStyles, Alignment]
+
+export class ResolvedGroup {
+  constructor(readonly item: MenuItem,
+              readonly content: readonly (MenuItem | ResolvedGroup)[]) {}
+}
+
+export function resolveMenu(
+  items: readonly MenuItem[],
+  template: TemplateGroup | readonly TemplateGroup[] = TemplateGroup.of(Top)
+): readonly (MenuItem | ResolvedGroup)[] {
+  let used = new Set<MenuItem>()
+  function resolve(template: MenuItem | TemplateGroup, content: readonly (MenuItem | "...")[] | null,
+                   target: (MenuItem | ResolvedGroup)[]) {
+    if (template instanceof TemplateGroup) {
+      resolve(template.item, template.content, target)
+    } else {
+      if (used.has(template)) return
+      used.add(template)
+      if (template.type == "select" || template.type == "group") {
+        let innerTarget: (MenuItem | ResolvedGroup)[] = template.type == "select" ? [] : target
+        for (let elt of content || ["..."]) {
+          if (elt === "...") {
+            let found: MenuItem[] = items.filter(i => i.parent == template)
+            for (let item of found.sort((a, b) => (a.rank ?? 100) - (b.rank ?? 100))) resolve(item, null, innerTarget)
+          } else {
+            resolve(elt, null, innerTarget)
+          }
+        }
+        if (innerTarget != target && innerTarget.length)
+          target.push(new ResolvedGroup(template, innerTarget))
+      } else {
+        target.push(template)
+      }
+    }
+  }
+
+  let top: (MenuItem | ResolvedGroup)[] = []
+  if (Array.isArray(template)) for (let elt of template) resolve(elt, null, top)
+  else resolve(template as TemplateGroup, null, top)
+  return top
 }
--- menu/src/menu.ts
+++ menu/src/menu.ts
@@ -1,8 +1,11 @@
 import {showPanel, EditorView, ViewUpdate} from "@wordgard/view"
 import {EditorState, Extension} from "@wordgard/state"
-import {MenuItem, staticMenu} from "./item"
+import {MenuLabel, MenuItem, MenuButton, MenuSelect, resolveMenu, staticMenu, ResolvedGroup} from "./item"
 
-type BarElement = BarButton | BarDropDown
+// FIXME use view.dom.ownerDocument
+// FIXME redraw when menu-item facets change
+
+type BarElement = BarButton | BarSelect
 
 let nextID = 0
 
@@ -12,30 +15,35 @@ function id(prefix: string) {
 
 const SVG = "http://www.w3.org/2000/svg"
 
+function labelButton(state: EditorState, button: HTMLElement, label: MenuLabel) {
+  if (typeof label == "string") {
+    button.textContent = state.phrase(label)
+  } else {
+    button.textContent = ""
+    let svg = button.appendChild(document.createElementNS(SVG, "svg"))
+    svg.classList.add("wg-icon")
+    svg.setAttribute("viewBox", "0 0 16 16")
+    let path = svg.appendChild(document.createElementNS(SVG, "path"))
+    path.setAttribute("d", label.icon)
+  }
+}
+
 class BarButton {
   dom: HTMLElement
   hidden = false
   active = false
   enabled = true
 
-  constructor(readonly item: MenuItem, state: EditorState) {
-    this.dom = document.createElement("wg-menu-item")
-    this.dom.role = "button"
-    this.dom.id = id("wg-menubar-item")
-    if (typeof item.label == "string") {
-      this.dom.textContent = state.phrase(item.label)
-    } else {
-      this.dom.className = "wg-icon"
-      let svg = this.dom.appendChild(document.createElementNS(SVG, "svg"))
-      svg.setAttribute("viewBox", "0 0 16 16")
-      let path = svg.appendChild(document.createElementNS(SVG, "path"))
-      path.setAttribute("d", item.label.icon)
-    }
-    let desc = state.phrase(item.description)
-    this.dom.title = desc
-    this.dom.setAttribute("aria-label", desc)
-    this.update(state)
+  constructor(readonly item: MenuButton, view: EditorView) {
+    this.dom = document.createElement("button")
+    this.dom.className = "wg-menu-button"
+    this.dom.tabIndex = -1
+    labelButton(view.state, this.dom, item.label)
+    this.dom.setAttribute("aria-label", this.dom.title = view.state.phrase(item.description))
+    this.update(view.state)
   }
+
+  get focusDOM() { return this.dom }
 
   run(view: EditorView) {
     if (!this.hidden && this.enabled)
@@ -81,30 +89,135 @@ class BarButton {
   }
 }
 
-class BarDropDown {
-  dom: HTMLElement
-  list: HTMLElement
-  elts: readonly BarElement[]
+// FIXME handle hidden items
+function setSelected(target: {elts: readonly BarElement[], selected: number}, index: number, focus = true) {
+  if (focus && index >= 0) target.elts[index].focusDOM.focus()
+  if (index != target.selected) {
+    if (index > -1) {
+      let dom = target.elts[index].focusDOM
+      dom.setAttribute("aria-selected", "true")
+      dom.tabIndex = 0
+    }
+    if (target.selected >= 0) {
+      let dom = target.elts[target.selected].focusDOM
+      dom.removeAttribute("aria-selected")
+      dom.tabIndex = -1
+    }
+    target.selected = index
+  }
+}
 
-  constructor(items: readonly MenuItem[], state: EditorState) {
-    this.dom = document.createElement("wg-menu-item")
-    this.dom.role = "button"
-    this.dom.setAttribute("aria-haspopup", "true")
-    this.dom.setAttribute("aria-expanded", "false")
-    this.dom.setAttribute("aria-controls", "FIXME")
-    this.list = document.createElement("wg-menu-list")
-    this.dom.appendChild(this.list)
-    this.elts = items.map(i => new BarButton(i, state))
-    for (let elt of this.elts) this.list.appendChild(elt.dom)
+
+class BarSelect {
+  dom: HTMLElement
+  button: HTMLElement
+  list: HTMLElement
+  open = false
+  selected = -1
+  activeChild: BarElement | null | undefined = null
+
+  constructor(readonly item: MenuSelect, readonly elts: readonly BarElement[], readonly view: EditorView) {
+    this.dom = document.createElement("wg-menu-select")
+    this.button = this.dom.appendChild(document.createElement("button"))
+    this.button.className = "wg-menu-button"
+    this.button.setAttribute("aria-haspopup", "true")
+    this.button.setAttribute("aria-expanded", "false")
+    this.button.setAttribute("aria-label", this.button.title = view.state.phrase(item.description))
+    if (item.label) labelButton(view.state, this.button, item.label)
+    this.list = this.dom.appendChild(document.createElement("wg-menu-list"))
+    this.list.style.display = "none"
+    this.list.role = "menu"
+    this.list.id = id("wg-popup")
+    this.list.setAttribute("aria-label", this.dom.title)
+    this.button.setAttribute("aria-controls", this.list.id)
+    for (let elt of elts) {
+      this.list.appendChild(elt.dom)
+      elt.dom.role = "menuitemradio"
+    }
+    this.list.addEventListener("mousedown", event => this.click(event))
+    this.list.addEventListener("keydown", event => this.key(event))
+    this.list.addEventListener("focusout", () => {
+      setTimeout(() => {
+        if (!this.list.contains(document.activeElement)) this.setOpen(false)
+      }, 20)
+    })
+    this.updateActive(view.state)
+    setSelected(this, 0, false)
   }
 
+  get focusDOM() { return this.button }
+
   run(view: EditorView) {
-    // FIXME
+    this.setOpen(!this.open)
+  }
+
+  setOpen(open: boolean) {
+    if (this.open == open) return
+    this.open = open
+    this.button.setAttribute("aria-expanded", String(open))
+    if (open) {
+      this.list.style.display = "block"
+      setSelected(this, this.activeChild ? this.elts.indexOf(this.activeChild) : this.selected, true)
+    } else {
+      this.list.style.display = "none"
+    }
   }
 
   update(state: EditorState) {
     for (let elt of this.elts) elt.update(state)
+    this.updateActive(state)
   }
+
+  updateActive(state: EditorState) {
+    if (this.item.label) return
+    let active = this.elts.find(e => e instanceof BarButton && e.active)
+    if (active !== this.activeChild) {
+      this.activeChild = active
+      labelButton(state, this.button, (active?.item as MenuButton)?.label || this.item.defaultLabel) // FIXME
+    }
+  }
+
+  key(event: KeyboardEvent) {
+    if (event.ctrlKey || event.altKey || event.metaKey) return
+    if (event.key == "ArrowUp") {
+      setSelected(this, (this.selected ? this.selected : this.elts.length) - 1)
+    } else if (event.key == "ArrowDown") {
+      setSelected(this, this.selected == this.elts.length - 1 ? 0 : this.selected + 1)
+    } else if (event.key == "ArrowLeft" || event.key == "ArrowRight") {
+      this.setOpen(false)
+      // FIXME can we be sure parent is appropriate here?
+      this.dom.parentElement!.focus()
+      return
+    } else if (event.key == "Home" || event.key == "End") {
+      setSelected(this, event.key == "Home" ? 0 : this.elts.length - 1)
+    } else if (event.key == " " || event.key == "Enter") {
+      this.focusDOM.focus()
+      this.setOpen(false)
+      this.elts[this.selected].run(this.view)
+    } else if (event.key == "Escape") {
+      this.focusDOM.focus()
+      this.setOpen(false)
+    } else {
+      return
+    }
+    event.stopPropagation()
+    event.preventDefault()
+  }
+
+  click(event: MouseEvent) {
+    let i = this.elts.findIndex(e => e.dom.contains(event.target as HTMLElement))
+    if (i < 0) return
+    setSelected(this, i)
+    this.elts[i].run(this.view)
+    event.preventDefault()
+  }
+}
+
+function instantiate(item: MenuItem | ResolvedGroup, view: EditorView): BarElement {
+  if (item instanceof ResolvedGroup) return new BarSelect(item.item as MenuSelect /* FIXME */,
+                                                          item.content.map(i => instantiate(i, view)), view)
+  else if (item.type == "button") return new BarButton(item, view)
+  else throw new Error("No implementation for menu item type " + item.type)
 }
 
 const menuPlugin = showPanel.of(view => new MenuBar(view))
@@ -117,13 +230,12 @@ class MenuBar {
   constructor(readonly view: EditorView) {
     this.dom = document.createElement("wg-menubar")
     this.dom.role = "toolbar"
-    this.dom.tabIndex = 0
     this.dom.setAttribute("aria-controls", view.contentDOM.id)
     this.dom.addEventListener("keydown", this.key.bind(this))
     this.dom.addEventListener("mousedown", this.click.bind(this))
-    this.elts = staticMenu.map(i => new BarButton(i, view.state))
+    this.elts = resolveMenu(staticMenu).map(i => instantiate(i, view))
     for (let elt of this.elts) this.dom.appendChild(elt.dom)
-    this.setSelected(0)
+    if (this.elts.length) setSelected(this, 0, false)
   }
 
   update(update: ViewUpdate) {
@@ -133,11 +245,11 @@ class MenuBar {
   key(event: KeyboardEvent) {
     if (event.ctrlKey || event.altKey || event.metaKey) return
     if (event.key == "ArrowLeft") {
-      this.setSelected((this.selected ? this.selected : this.elts.length) - 1)
+      setSelected(this, (this.selected ? this.selected : this.elts.length) - 1)
     } else if (event.key == "ArrowRight") {
-      this.setSelected(this.selected == this.elts.length - 1 ? 0 : this.selected + 1)
+      setSelected(this, this.selected == this.elts.length - 1 ? 0 : this.selected + 1)
     } else if (event.key == "Home" || event.key == "End") {
-      this.setSelected(event.key == "Home" ? 0 : this.elts.length - 1)
+      setSelected(this, event.key == "Home" ? 0 : this.elts.length - 1)
     } else if (event.key == " " || event.key == "Enter") {
       this.elts[this.selected].run(this.view)
     } else {
@@ -149,18 +261,9 @@ class MenuBar {
   click(event: MouseEvent) {
     let i = this.elts.findIndex(e => e.dom.contains(event.target as HTMLElement))
     if (i < 0) return
-    this.setSelected(i)
+    setSelected(this, i)
     this.elts[i].run(this.view)
     event.preventDefault()
-  }
-
-  // FIXME somehow skip hidden items
-  setSelected(index: number) {
-    if (index == this.selected) return
-    if (this.selected >= 0) this.elts[this.selected].dom.removeAttribute("aria-selected")
-    let elt = this.elts[this.selected = index]
-    elt.dom.setAttribute("aria-selected", "true")
-    this.dom.setAttribute("aria-activedescendant", elt.dom.id)
   }
 
   get top() { return true }
@@ -180,17 +283,19 @@ const theme = EditorView.baseTheme({
     // FIXME light/dark colors
     color: "#555",
     borderBottom: "1px solid var(--wg-border-color)",
-    outline: 0,
-
-    "&:focus [aria-selected=true]": {
-      outline: "1.5px solid var(--wg-menu-highlight)"
-    }
   },
 
-  "wg-menu-item": {
+  ".wg-menu-button:focus": {
+    outline: "1.5px solid var(--wg-menu-highlight)"
+  },
+
+  ".wg-menu-button": {
     display: "block",
+    border: 0,
     padding: "1px",
     borderRadius: "2px",
+    backgroundColor: "transparent",
+    font: "inherit",
     height: "var(--wg-menu-item-size)",
     "&[aria-disabled]": {
       opacity: "0.4"
@@ -201,14 +306,27 @@ const theme = EditorView.baseTheme({
     "&:hover": {
       backgroundColor: "#88888827",
     },
-    "&.wg-icon": {
-      "& svg": {
-        fill: "currentColor",
-        width: "var(--wg-menu-item-size)",
-        height: "var(--wg-menu-item-size)",
-      }
-    },
-  }
+  },
+
+  "svg.wg-icon": {
+    fill: "currentColor",
+    width: "var(--wg-menu-item-size)",
+    height: "var(--wg-menu-item-size)",
+  },
+
+  "wg-menu-select": {
+    position: "relative",
+  },
+
+  "wg-menu-list": {
+    borderRadius: "2px",
+    padding: "1.5px",
+    backgroundColor: "var(--wg-panel-color)",
+    position: "absolute",
+    left: "-1.5px",
+    top: "100%",
+    border: "1px solid var(--wg-border-color)",
+  },
 })
 
 export function menuBar(): Extension {
