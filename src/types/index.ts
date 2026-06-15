512a107b83a0425969902df8b7bd917543ddc306
Marijn Haverbeke <marijn@haverbeke.berlin>
2026-06-15 19:01:26 +0200

    Document types/index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,16 @@
+/// This module provides a collection of basic schema elements. It is
+/// possible to define your own custom elements, but often convenient
+/// to use the ones provided here, which come with support extensions
+/// in the [wordgard/schema](#schema) module.
+///
+/// This module depends on only on [wordgard/doc](#doc) and is kept
+/// separate from [wordgard/schema](#schema) so that you can access
+/// the types without loading the editor code, when appropriate.
+///
+/// ### Block Structure
+///
+/// Nodes and marks for structuring documents on the block level.
+
 export {
   Paragraph,
   Heading,
@@ -9,22 +22,18 @@ export {
   OrderedList,
   BulletList,
   HorizontalRule,
-  LineBreak,
-  Cell,
-  HeaderCell,
-  BlockCell,
-  BlockHeaderCell,
-  TableRow,
-  Table,
-  ColSpan,
-  RowSpan,
-  Image,
-  Figure,
-  CaptionedFigure,
-  ImageAlt,
-  ImageSize,
   Alignment,
   Direction,
+  Doc,
+  InlineDoc,
+} from "./schema"
+
+/// ### Inline Structure
+///
+/// Marks and nodes for inline content.
+
+export {
+  LineBreak,
   Emphasis,
   Strong,
   Underline,
@@ -35,7 +44,33 @@ export {
   Code,
   Color,
   BackgroundColor,
-  Doc,
-  InlineDoc
 } from "./schema"
 
+/// ### Images
+///
+/// Image types and their marks.
+
+export {
+  Image,
+  Figure,
+  CaptionedFigure,
+  ImageAlt,
+  ImageSize,
+} from "./schema"
+
+/// ### Tables
+///
+/// Nodes and marks used to define tables. See
+/// [wordgard/table](#table) for the supporting extensions that make
+/// table editing possible.
+
+export {
+  Cell,
+  HeaderCell,
+  BlockCell,
+  BlockHeaderCell,
+  TableRow,
+  Table,
+  ColSpan,
+  RowSpan,
+} from "./schema"
