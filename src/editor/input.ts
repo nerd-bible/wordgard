b41e3acddbf4d3489df2ae1a810ed807b4c20d6f
Marijn Haverbeke <marijn@haverbeke.berlin>
2026-07-15 16:29:57 +0200

    Improve querying of DOM ranges after Chrome Android DOM mangling
    FIX: Avoid confusion about selection and deletion ranges when Android
    voice typing or a sloppy virtual keyboard fires a burst of input events.
    
    Issue https://code.haverbeke.berlin/wordgard/wordgard/issues/4

--- src/editor/input.ts
+++ src/editor/input.ts
@@ -6,7 +6,7 @@ import {Command, undo, redo, insertLineBreak, enter, insertText,
         transposeChars, deleteSelection, setAlignment, setDirection} from "wordgard/command"
 import {Wordgard} from "./editor"
 import browser from "./browser"
-import {getSelection, scrollableParents, DOMNode, textNodeBefore, textNodeAfter} from "./dom"
+import {getSelection, scrollableParents, DOMNode, textNodeBefore, textNodeAfter, domIndex} from "./dom"
 import {readClipboard, writeClipboard} from "./clipboard"
 import {eqArray, logException} from "./util"
 import {Tile, CoordPos} from "./tile"
@@ -68,6 +68,7 @@ export class InputState {
   pendingComposition: {from: number, to: number, text: string} | null = null
   // Used in the hack to handle weird Chrome Android deletions
   pendingDeletion: {from: number, to: number} | null = null
+  modifiedTextNodes: Set<Node> = new Set
   wrappingComposition: Mark.Set | null = null
 
   mouseSelection: MouseSelection | null = null
@@ -153,7 +154,11 @@ export class InputState {
     if (this.mouseSelection) this.mouseSelection.update(update)
     if (this.draggedContent && update.docChanged) this.draggedContent = this.draggedContent.map(update.changes, update.state)
     if (update.transactions.length) this.lastKeyCode = 0
-    if (this.composing) this.composing.targetPos = update.changes.mapPos(this.composing.targetPos, -1)
+    this.modifiedTextNodes.clear()
+    if (this.composing) {
+      this.composing.targetPos = update.changes.mapPos(this.composing.targetPos, -1)
+      if (this.composing.target) this.modifiedTextNodes.add(this.composing.target)
+    }
   }
 
   findComposition(): {target: Text, targetPos: number} | null {
@@ -176,9 +181,26 @@ export class InputState {
       let pos = this.wg.docTile.posBeforeDOM(newTarget)
       if (pos == null) return comp.target = null
       comp.target = newTarget
+      this.modifiedTextNodes.add(newTarget)
       comp.targetPos = this.wg.viewState.mapPosPending(pos, -1)
     }
     return comp as {target: Text, targetPos: number}
+  }
+
+  markModifiedNodes(range: StaticRange) {
+    let {startContainer: start, startOffset: startOff, endContainer: end, endOffset: endOff} = range
+    for (;;) {
+      if (start.nodeType == 3 && !this.modifiedTextNodes.has(start))
+        this.modifiedTextNodes.add(start)
+      if (start == end && (start.nodeType != 1 || startOff == endOff)) break
+      if (start.nodeType != 1 || startOff == start.childNodes.length) {
+        startOff = domIndex(start) + 1
+        start = start.parentNode!
+      } else {
+        start = start.childNodes[startOff]
+        startOff = 0
+      }
+    }
   }
 
   connect() {
@@ -565,22 +587,28 @@ const inputTypeCommands: {[inputType: string]: Command.Bound | Command} = {
   formatJustifyRight: Command.bind(setAlignment, "right")
 }
 
+function interpretDOMPosition(wg: Wordgard, node: Node, offset: number, bias: -1 | 1) {
+  if (node.nodeType == 3 && wg.viewState.pending.length && wg.inputState.modifiedTextNodes.has(node)) {
+    let parent = wg.docTile.nearest(node)
+    if (parent?.isText && parent.dom == node) {
+      let start = parent.posAtStart
+      return wg.viewState.mapPosPending(start, bias) + offset
+    }
+  }
+  let pos = wg.docTile.posFromDOM(node, offset)
+  return wg.viewState.mapPosPending(pos, bias)
+}
+
 function inputEventRange(event: InputEvent, wg: Wordgard, preferSel = false) {
   let range = event.getTargetRanges()[0]
-  let from = wg.docTile.posFromDOM(range.startContainer, range.startOffset, -1)
-  let to = range.collapsed ? from : wg.docTile.posFromDOM(range.endContainer, range.endOffset, 1)
+  let from = interpretDOMPosition(wg, range.startContainer, range.startOffset, -1)
+  let to = interpretDOMPosition(wg, range.endContainer, range.endOffset, -1)
+
   let {pending} = wg.viewState
-  if (pending.length) {
-    let comp = wg.inputState.composing
-    if (preferSel && !comp && from == to) {
-      let fromMin = wg.viewState.mapPosPending(from, -1), fromMax = wg.viewState.mapPosPending(from, 1)
-      if (fromMin <= wg.state.selection.from && fromMax >= wg.state.selection.to)
-        return wg.state.selection
-    }
-    if (comp && comp.target == range.startContainer) from = comp.targetPos + range.startOffset
-    else from = wg.viewState.mapPosPending(from, 1)
-    if (comp && comp.target == range.endContainer) to = comp.targetPos + range.endOffset
-    else to = wg.viewState.mapPosPending(to, 1)
+  if (pending.length && preferSel && !wg.inputState.composing && from == to) {
+    let fromMax = interpretDOMPosition(wg, range.startContainer, range.startOffset, 1)
+    if (from <= wg.state.selection.from && fromMax >= wg.state.selection.to)
+      return wg.state.selection
   }
   return {from, to}
 }
@@ -697,6 +725,7 @@ const baseHandlers: {[e in keyof HTMLElementEventMap]?: (wg: Wordgard, event: HT
       // twice, we defer handling to the input handler.
       if (browser.android && browser.chrome && (type == "deleteContentBackward" || type == "deleteContentForward")) {
         wg.inputState.pendingDeletion = inputEventRange(event, wg)
+        wg.inputState.markModifiedNodes(event.getTargetRanges()[0])
         LOG_input && console.log("beforeinput", type, wg.inputState.pendingDeletion, "(chrome)")
         return false
       }
