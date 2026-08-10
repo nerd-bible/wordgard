9721b2c76f093ba81a0fca83b9d7e61ff579313d
Marijn Haverbeke <marijn@haverbeke.berlin>
2026-08-10 12:36:10 +0200

    Drop accidentally commited demo content

--- demo/demo.ts
+++ demo/demo.ts
@@ -5,7 +5,7 @@ import {tables} from "wordgard/table"
 
 ;(window as any).wg = Wordgard.create({
   parent: document.body,
-  doc: `<h2>Demo Content</h2><pre></pre><p>A <em>paragraph</em>.</p><pre data-language=JavaScript>let x = 100</pre>`,
+  doc: `<h2>Demo Content</h2><p>A <em>paragraph</em>.</p>`,
   config: [
     fullSchema(),
     codeBlockLanguage({languages: ["JavaScript", "TypeScript", "Markdown", "C++", "Python"]}),
